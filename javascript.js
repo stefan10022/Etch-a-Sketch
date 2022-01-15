41145f2ef0a1c5ef60dffb5abc534ba0047d311b
Stefan Konstantinovikj <stefan10022@protonmail.com>
2022-01-15 22:09:49 +0100

    Fixed screen bug and added prompt text

--- javascript.js
+++ javascript.js
@@ -1,9 +1,17 @@
-
 const screen = document.querySelector('#screen');
 const generate = document.querySelector('#generate');
+let size = 0;
+
+function getSize () {
+    size = prompt('Enter grid size (example: 16 gives you a 16x16 grid).');
+    if (isNaN(size) || size < 1 || size > 99 || size % 1 != 0) {
+        alert('The grid size must be an integer between 1 and 100.')
+        getSize();
+    }
+}
 
 generate.addEventListener('click', () => {
-    size = prompt('');
+    getSize();
     removeGrid();
     createGrid();
     screen.style.border = "1px solid black";  
@@ -17,8 +25,8 @@ function createGrid () {
     div.addEventListener('mouseenter', () => {
         div.style.backgroundColor = 'black';
     });
-    div.style.height = `${600/size}px`;
-    div.style.width = `${600/size}px`;
+    div.style.height = `${900/size}px`;
+    div.style.width = `${900/size}px`;
     screen.appendChild(div);
     }   
 }
